ff3976aa80d8da5822dabcf565ade47256bc935f
mayorwise001 <tomosorijoseph@yahoo.com>
2022-09-08 19:52:20 -0700

    ok

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+    * main - Entry point
+    * Return: 1 on success
+    */
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpae. 2015-10-19\n");
+	return (1);
+}
