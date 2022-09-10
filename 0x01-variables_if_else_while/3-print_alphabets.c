3d75ca409d5a53ec187997e9be8edcdc476293a9
mayorwise001 <tomosorijoseph@yahoo.com>
2022-09-09 22:08:44 -0700

    ok

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -15,15 +15,15 @@ int main(void)
 {
 	int i;
 
-	for(i =65; i<= 90; i++);
+	for (i = 65; i <= 90; i++);
 	{
 		putchar(i);
 	}
-	for(i=97; i<= 122; i++)
+	for (i=97; i <= 122; i++)
 	{ 
 		putchar(i);
 	}
-	putchar(10)
+	putchar(10);
 	return (0)
 }
 			
