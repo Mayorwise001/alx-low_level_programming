3a59edf485f4bc98a069c5789b807b98902e6227
mayorwise001 <tomosorijoseph@yahoo.com>
2022-09-10 01:39:54 -0700

    ok

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,29 +1,22 @@
 #include <stdio.h>
 
 /**
- *
- * main - print the letters of the alphabet
- *  Description: print the letters of the alphabet
- *  Return: Always 0 (Success)
- *
- */
-
+    * main - print the letters of the alphabet
+    *  Description: print the letters of the alphabet
+    * Return: Always 0 (Success)
+    */
 
 
 int main(void)
 
 {
-	int i;
+	int i = 65;
 
-	for (i = 65; i <= 90; i++);
-	{
+	while (i < 91)
+{
 		putchar(i);
+		i++;
 	}
-	for (i=97; i <= 122; i++)
-	{ 
-		putchar(i);
-	}
-	putchar(10);
-	return (0)
+		putchar(10);
+	return (0);
 }
-			
