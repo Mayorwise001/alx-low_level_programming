5c3cfa8f481823388482a37632054b66f11527ac
mayorwise001 <tomosorijoseph@yahoo.com>
2022-09-09 18:57:03 -0700

    ok

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -20,11 +20,11 @@ int main(void)
 	printf("Enter a number:");
 	scanf("%d",&num);
 	if(num>0)
-		printf("%d is positive",num);
+		printf("%d is positive\n",num);
 	if(num<0)
-		printf("%d is negative",num);
+		printf("%d is negative\n",num);
 	if((num=0))
-		printf("%d is zero",num);
+		printf("%d is zero\n",num);
 	return (0);
 
 
