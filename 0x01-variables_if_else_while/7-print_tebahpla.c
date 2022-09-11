b7ce7c01f19900ab77d87c5cecebfe7ed3cd1760
mayorwise001 <tomosorijoseph@yahoo.com>
2022-09-11 13:28:57 -0700

    ok

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -11,12 +11,10 @@
 int main(void)
 
 {
-	int i;
+	char i;
 
-	for (i = 'z'; i >= 'a; i-- )
-		{
-			putchar(i);
-	}
+	for (i = 'z' ; i >= 'a' ; i--)
+		putchar(i);
 	putchar('\n');
 	return (0);
 }
