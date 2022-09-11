08dedea35e27c8fd2810c7d8da2723558175abeb
mayorwise001 <tomosorijoseph@yahoo.com>
2022-09-11 09:00:51 -0700

    ok

---
+++ 0x01-variables_if_else_while/6-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+    *main - print base numbers
+    *
+    *Return: only new numbers
+    */
+
+int main(void)
+{
+int i = 0;
+
+	while (i < 10)
+{
+
+		putchar(i);
+		i++;
+		putchar('\n');
+}
+	return (0);
+}
