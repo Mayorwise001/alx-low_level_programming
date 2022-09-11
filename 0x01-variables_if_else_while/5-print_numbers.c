fba3eb84db906e912d263d49092b8ac7f3741b1d
mayorwise001 <tomosorijoseph@yahoo.com>
2022-09-11 07:56:47 -0700

    ok

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
-/** main - pnt base numbers
+
+/**
+    *main - print base numbers
     *
-    * Return: only new numbers
-    */ 
+    *Return: only new numbers
+    */
 
 int main(void)
 {
 	int i = 0;
 		while (i < 10)
 			i++;
-{
+		{
 	printf("%d this\n", i);
 	return (0);
 }
