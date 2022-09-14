88b20f3cadbd99424103effbdbc888118d655a9c
mayorwise001 <tomosorijoseph@yahoo.com>
2022-09-14 16:47:59 -0700

    ok

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -17,28 +17,20 @@ void times_table(void)
 	for (r = 0; r <= 9; r++)
 	{
 		_putchar('0');
-		_putchar(',');
-		_putchar(' ');
 
 		for (c = 1; c <= 9; c++)
 		{
+			_putchar(',');
+			_putchar(' ');
 			d = (r * c);
 
-			if ((d / 10) > 0)
-			{
-				_putchar((d / 10) + '0');
-			}
-			else
-			{		_putchar(' ');
-			}
-					_putchar((d % 10) + '0');
-			if (c < 9)
-			{
-				_putchar(',');
+			if (d <= 9)
 				_putchar(' ');
-			}
+			else
+				_putchar((d / 10) + '0');
+			_putchar((d % 10) + '0');
 			}
 
 }
-			_putchar('\n');
+		_putchar('\n');
 		}
