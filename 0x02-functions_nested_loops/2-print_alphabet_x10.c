dfa718a31151d5fe7149f33b5677d64681ded2ba
mayorwise001 <tomosorijoseph@yahoo.com>
2022-09-14 08:06:56 -0700

    ok

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* print_alphabet -writes the charate
+* Description: The charter to print
+* Return: void
+*/
+
+
+void print_alphabet_x10(void)
+{
+int i;
+
+for (i = 'a'; i <= 'z'; i++)
+{
+	_putchar(i);
+	_putchar(10);
+}
+	retunr (0);
+}
