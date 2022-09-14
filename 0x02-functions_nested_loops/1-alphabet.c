db724edb2b6f63d7e734d53b70e57f0b8bc75167
mayorwise001 <tomosorijoseph@yahoo.com>
2022-09-14 07:58:18 -0700

    ok

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* main -writes the charate
+* Description: The charter to print
+* Return: on the success 1.
+*/
+
+
+int void print_alphabet(void)
+{
+int i;
+
+for (i = 'a'; i <= 'z'; i++)
+{
+	_putchar(i);
+}
+
+_putchar('\n');
+}
