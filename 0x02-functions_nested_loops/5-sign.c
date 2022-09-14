5735cdd1de93299084bc324bcba26dbecafb222b
mayorwise001 <tomosorijoseph@yahoo.com>
2022-09-14 08:52:51 -0700

    ok

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+* print_sign - prints 1 or 0 depending on input
+* @c: first parameter
+* Description: The charter to print
+* Return: always (0)
+*/
+
+
+int print_sign(int c)
+{
+	if (c > 0)
+	{
+		-putchar('+');
+		return (1);
+	}
+
+	if (c == 0)
+	{
+		-putchar('0');
+		return (0);
+	}
+
+	_putchar('-');
+	return (-1);
+}
