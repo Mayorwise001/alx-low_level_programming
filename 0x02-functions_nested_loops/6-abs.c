16c06badb4c357b7388a00c2ed98af010cf87df6
mayorwise001 <tomosorijoseph@yahoo.com>
2022-09-14 09:38:08 -0700

    ok

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* _abs - main function
+*
+*@n: character to compare
+*
+*Return: 1 fi positive, 0 if zero, -1 if negative
+*
+*/
+
+int _abs(int n)
+
+{
+
+	if (n > 0)
+		return (n);
+	}
+	else
+		return (n * -1);
+}
