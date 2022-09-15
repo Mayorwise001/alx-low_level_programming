a26456b7d5be68909d55c8de1312efdce4441b96
mayorwise001 <tomosorijoseph@yahoo.com>
2022-09-14 19:43:24 -0700

    ok

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+*add - main function
+*
+*@i: prints a list of time
+* @k: row, cone = column, d = digits of current result
+*Return: times table.
+*
+*/
+
+int add(int i, int k)
+{
+	return (i + k);
+}
