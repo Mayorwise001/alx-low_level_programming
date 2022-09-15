5ba02bce2a174732a6a9fe510961997f22601980
mayorwise001 <tomosorijoseph@yahoo.com>
2022-09-14 19:54:45 -0700

    ok

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+*print_to_98 - main function
+*
+*@n: prints a list of time
+* : row, cone = column, d = digits of current result
+*Return: times table.
+*
+*/
+
+void print_to_98(int n)
+{
+	if (n < 98)
+	{
+		for (n = n; n < 98; n++)
+			printf(" %d, ", n);
+		printf("%d\n", 98);
+	}
+	else
+	{
+		for (n = n; n > 98; n--)
+			printf("%d, ", n);
+		printf("%d\n", 98);
+
+	return (i + k);
+}
