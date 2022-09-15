d11d7af978ac2c7093f507d7587d7265e1227216
mayorwise001 <tomosorijoseph@yahoo.com>
2022-09-15 07:41:40 -0700

    ok

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -6,19 +6,23 @@
 */
 
 int main(void)
+
+
 {
 	int i;
 
 	printf("infinite loop incoming :(\n");
-	
+
 	i = 0;
+	
 	/**
 	 * while (1 < 10)
  	 * {
  	 * putchar(i);
  	 * }
  	 */
- 	printf("Infinite loop avoided! \\o/\n");
+
+	 printf("Infinite loop avoided! \\o/\n");
 	
 	return (0);
 }
