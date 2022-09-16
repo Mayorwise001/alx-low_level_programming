4cb91f0ec5bd7895b3f9d195a3174d0c9cd58086
mayorwise001 <tomosorijoseph@yahoo.com>
2022-09-16 05:37:24 -0700

    ok

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+*print_line - checks if a character is uppercase or n
+*@n: parameter
+*Return: returns result
+*/
+
+void print_line(int n)
+{
+	while (n-- > 0)
+	}
+		putchar('_');
+
+	}
+	putchar('\n');
+}
