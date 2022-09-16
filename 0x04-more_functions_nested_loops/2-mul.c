5ac05e0bcb6416f8230375b068a8bc61b36e7d29
mayorwise001 <tomosorijoseph@yahoo.com>
2022-09-16 04:47:54 -0700

    ok

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -4,7 +4,7 @@
 *mul - checks if a character is uppercase or not
 *@a: charcter to be tested
 *@b: it is ok
-*Return: 1 whether it is, 0 otherwise
+*Return: returns result
 */
 
 int mul(int a, int b)
