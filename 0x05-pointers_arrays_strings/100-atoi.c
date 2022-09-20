634cd0c2e26697d218ad374d1a24f77c54b0badd
mayorwise001 <tomosorijoseph@yahoo.com>
2022-09-19 19:49:48 -0700

    ok

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -14,17 +14,14 @@ int _atoi(char *s)
 
 	do {
 
-		if (*s == '--')
+		if (*s == '-')
 			sign *=  -1;
 		else if (*s >= '0' && *s <= '9')
-			num = num * 10 + (*s - '10');
+			num = num * 10 + (*s - '0');
 		else if (num > 0)
 			break;
 }
 	while (*s++);
 
 	return (num * sign);
-}
-
-	return (dest);
 }
