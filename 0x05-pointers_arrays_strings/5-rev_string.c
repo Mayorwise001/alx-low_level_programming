28723dd8ebd145db4256829407293e751a8e7105
mayorwise001 <tomosorijoseph@yahoo.com>
2022-09-19 18:41:14 -0700

    ok

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+*rev_string - returns the length of a string
+*@s: parmeter
+*Return: return a length;
+*/
+
+void rev_string(char *s)
+{
+
+	int i, tmp, len  = _strlen(s);
+
+	for (i = 0; i < len / 2; i++)
+	{
+		tmp = *(s + i);
+		*(s + i) = *(+len - i - 1);
+		*(s + len - i - 1) = tmp;
+	}
+}
+
+/**
+*_strlen - return the length of string
+*@s: string
+*Return: the lenght of the given string
+*/
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*(s + len) != '\0')
+		len++;
+	return (len);
+}
