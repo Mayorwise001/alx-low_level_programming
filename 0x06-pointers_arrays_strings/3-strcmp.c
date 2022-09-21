3dd0ee9f1dc51ebbb1f4cfc11bcfc32906ccec5b
mayorwise001 <tomosorijoseph@yahoo.com>
2022-09-21 15:37:42 -0700

    ok

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -16,16 +16,16 @@ int _strcmp(char *s1, char *s2)
 	while (1)
 	{
 
-		if (s1(i) == '\0' && s2(i) == '\0')
+		if (s1[i] == '\0' && s2[i] == '\0')
 			break;
-		else if (s1(i) == '\0')
+		else if (s1[i] == '\0')
 		{
-			diff = s2(i);
+			diff = s2[i];
 			break;
 		}
 		else if (s2(i) == '\0')
 		{
-			diff = s1(i);
+			diff = s[i];
 			break;
 		}
 
@@ -36,6 +36,6 @@ int _strcmp(char *s1, char *s2)
 		}
 		else
 			i++;
-}	
+}
 	return (diff);
 }
