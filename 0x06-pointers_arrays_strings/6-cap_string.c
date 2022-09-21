1ddd97b7a06d1c2cbe81a919200133e9ba20d86b
mayorwise001 <tomosorijoseph@yahoo.com>
2022-09-21 16:45:50 -0700

    ok

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+*_strlen - concatenates the string
+*@str: string
+*Return:  address to the string
+*/
+
+int _strlen(char *str)
+{
+	int len = 0;
+
+	while (*(s + len) != '\0')
+		len++;
+	return (len);
+}
+
+/**
+*cap_string - function that capitalize
+*@str: strng to capitalize
+*Return: returns the capitalized string
+*/
+
+char *cap_string(char *str)
+{
+	int index = 0;
+
+	while (str[++index])
+	{
+		while (!(str[index] >= 'a') && (str[index] <= 'z'))
+			index++;
+		if (str[index - 1] == ' ' ||
+					str[index - 1] == '\t' ||
+					str[index - 1] == '\n' ||
+					str[index - 1] == ',' ||
+					str[index - 1] == ';' ||
+					str[index - 1] == '.' ||
+					str[index - 1] == '!' ||
+					str[index - 1] == '?' ||
+					str[index - 1] == '"' ||
+					str[index - 1] == '(' ||
+					str[index - 1] == ')' ||
+					str[index - 1] == '{' ||
+					str[index - 1] == '}')
+			str[index] -= 32;
+		}
+		return (str);
+}
