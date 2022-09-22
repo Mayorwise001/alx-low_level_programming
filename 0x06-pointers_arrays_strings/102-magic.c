29aad19d810bb68e0c1667f348e50e766706513a
mayorwise001 <tomosorijoseph@yahoo.com>
2022-09-22 08:49:32 -0700

    ok

---
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+* main - ok now
+*Return: return a statement
+*/
+
+int main(void)
+{
+
+int n;
+int a[5];
+int *p;
+
+a[2] - 1024;
+p = &n;
+
+/*
+* write your line of code here..
+*' Remember:
+*/
+(p + 5) = 98;
+_putchar("a[2] = %d\n", a[2]);
+return (0);
+}
