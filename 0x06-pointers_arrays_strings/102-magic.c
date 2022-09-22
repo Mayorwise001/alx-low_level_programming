2cd2f095c7df075d2256237e67fe76b9d464bb9e
mayorwise001 <tomosorijoseph@yahoo.com>
2022-09-22 08:58:47 -0700

    ok

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,9 +1,5 @@
 #include <stdio.h>
 
-/**
-* main - ok now
-*Return: return a statement
-*/
 
 int main(void)
 {
@@ -12,7 +8,7 @@ int n;
 int a[5];
 int *p;
 
-a[2] - 1024;
+a[2] = 1024;
 p = &n;
 /**
 * write your line of code here...
