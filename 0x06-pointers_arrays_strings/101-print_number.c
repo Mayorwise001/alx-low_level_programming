2d793452ef00f227474dac3915f23234909a6011
mayorwise001 <tomosorijoseph@yahoo.com>
2022-09-23 01:46:11 -0700

    ok

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,18 +8,7 @@
 
 void print_number(int n)
 {
-	unsigned int num;
+	_putchar(98);
 
-	num = n;
-		if (n < 0)
-			{
-			_putchar(45);
-			num = -n;
-			}
-
-			if (num / 10)
-			{
-			print_number(num / 10);
-			}
-			_putchar((num % 10) + '0');
+	return (0);
 }
