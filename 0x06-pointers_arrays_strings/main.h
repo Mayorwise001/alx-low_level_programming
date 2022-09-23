5ac06f0a666b65e096b435170c2492627167828f
mayorwise001 <tomosorijoseph@yahoo.com>
2022-09-23 04:50:50 -0700

    ok

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -15,5 +15,4 @@ char *leet(char *);
 char *rot13(char *s);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 char *add_strings(char *n1, char *n2, char *r, int r_index);
-_putchar;
 #endif /*MAIN_H*/
