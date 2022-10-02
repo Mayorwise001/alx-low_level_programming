d2bd42c5d2df1ee9985b6f097baa616e9efcdb31
Mayowa Tomosori <67711110+Mayorwise001@users.noreply.github.com>
2022-10-02 16:28:38 +0100

    Update 0-whatsmyname.c
    ok

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,9 +7,8 @@
  * @argv: vector of arguments
  *Return: always 0
  */
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
-	UNUSED(argc);
 	printf("%s\n", argv[0]);
 	return (0);
 }
