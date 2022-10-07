5897accdf89efa15bcc1658ad6c7aea07286fd78
Mayowa Tomosori <67711110+Mayorwise001@users.noreply.github.com>
2022-10-07 05:00:07 +0100

    Update main.h

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,22 +1,10 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
-#include <stdio.h>
-#include <stdlib.h>
-
-
-int find_len(char *str);
-char *create_xarray(int size);
-char *iterate_zeroes(char *str);
-void get_prod(char *prod, char *mult, int digit, int zeroes);
-void add_nums(char *final_prod, char *next_prod, int next_len);
 
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-void _is_zero(char *argv[]);
-char *_initialize_array(char *ar, int lar);
-int _checknum(char *argv[], int n);
-int main(int argc, char *argv[]);
+
 #endif
