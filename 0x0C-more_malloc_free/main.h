3a44b55090d8f46e0aa3559610cfc18299676939
Mayowa Tomosori <67711110+Mayorwise001@users.noreply.github.com>
2022-10-07 04:44:03 +0100

    Update main.h

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,7 +1,8 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 #include <stdio.h>
-#include <ctype.h>
+#include <stdlib.h>
+
 
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
